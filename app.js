1d5ff168163be6697fe70f3fe519ecb854dfbdee
aseelalsaqer <>
2021-06-02 14:26:37 +0000

    adding JS file

---
+++ app.js
@@ -0,0 +1,15 @@
+var FirstName = "My"
+var LastName ="  Favourit Flower"
+var FavFlower = "  is Black Iirs"
+var X = FirstName + LastName +   FavFlower
+console.log(X)
+
+alert('Contact us for more information')
+
+var FavouritFlower = prompt("What is your favourit flower?")
+ 
+ if (FavouritFlower == 'Black Iris') {
+   document.write('<div>' + 'You are in the right place' + "</div>")
+ }
+
+
